c847c5f41b7a64c505a4e45e050f3110898532d9
Joseba Mirena <contact@josebamirena.com>
2017-10-24 17:33:44 +0200

    Main plugin structure

---
+++ v0.0.1/admin/language/en-gb/extension/payment/payapi.php
@@ -0,0 +1,60 @@
+<?php
+
+//Headings
+$_['heading_title']                = 'PayApi Payments';
+
+// Text
+$_['text_success']                 = 'PAyApi Payments settings have been updated successfully';
+
+$_['text_payment']                 = 'Payment';
+$_['text_payments']                = 'Payments';
+$_['text_account_status']          = 'status';
+$_['text_enabled']                 = 'Enabled';
+$_['text_disabled']                = 'Disabled';
+$_['text_edit']                    = 'Edit PayApi Payments';
+$_['text_payapi']		           = '<a target="_blank" href="http://payapi.io/"><img src="view/image/payment/payapi.png" alt="PayApi Payments" title="PayApi Payments" style="border: 1px solid #EEEEEE;" /></a>';
+
+// Tab(s)
+$_['tab_general']                  = 'General';
+$_['tab_status']                   = 'Order Status';
+
+// Account status(es)
+$_['account_unverified']           = 'unverified';
+$_['account_verified']             = 'verified';
+
+// Status(es)
+$_['status_processing']            = 'processing';
+$_['status_success']               = 'success';
+$_['status_canceled']              = 'canceled';
+$_['status_failed']                = 'failed';
+$_['status_chargeback']            = 'chargeback';
+
+// Label(s)
+$_['label_public_id']              = 'Public ID';
+$_['label_api_key']                = 'API key';
+$_['label_shipping']               = 'Default Shipping';
+$_['label_mode']                   = 'Mode';
+$_['label_instant_payments']       = 'Instant Payments';
+$_['label_debug']                  = 'Debug';
+$_['label_order']                  = 'Order';
+
+// Mode(s)
+$_['mode_1']                       = 'staging';
+$_['mode_2']                       = 'production';
+$_['mode_3']                       = 'demo';
+
+// Button(s)
+$_['button_instant']               = 'Instant Buy';
+$_['button_partial']               = 'From %sR%s/month';
+
+// Help
+$_['help_public_id']               = 'You can get your public ID in your payments backend';
+$_['help_api_key']                 = 'You can get your API key in your payments backend';
+$_['help_verified']                = 'You account is verified (%s)';
+$_['help_unverified']              = 'You account is unverified';
+
+// Error(s)
+$_['error_default']                = 'Oops, something went wrong, if you think this is an error please, contact us';
+$_['error_timeout']                = 'We are doing maintenance tasks, please try again in a few minutes';
+$_['error_account']                = 'Account values are not valid, please review your information and try again';
+$_['error_dependences']            = 'Missed dependencies: %s';
